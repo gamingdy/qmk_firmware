c0a75b6f4cf82be11f6ca5c5591aaee72e52c516
Gamingdy <christgik12@gmail.com>
2024-02-22 16:47:47 +0100

    feat: add all letter and number

--- keyboards/keychron/v3/iso_encoder/keymaps/via/keymap.c
+++ keyboards/keychron/v3/iso_encoder/keymaps/via/keymap.c
@@ -73,17 +73,21 @@ typedef struct{
     uint16_t timer;
 }key_index;
 
-key_index tablo[RGB_MATRIX_LED_COUNT] = {
-    {KC_Q, false, 34, 0}
+key_index key_array[RGB_MATRIX_LED_COUNT] = {
+    {KC_1, false, 17, 0},{KC_2, false, 18, 0},{KC_3, false, 19, 0},{KC_4, false, 20, 0},{KC_5, false, 21, 0},{KC_6, false, 22, 0},{KC_7, false, 23, 0},{KC_8, false, 24, 0},{KC_9, false, 25, 0},{KC_0, false, 26, 0},{KC_MINS, false, 27, 0},{KC_EQL, false, 28, 0},
+    {KC_Q, false, 34, 0}, {KC_W, false, 35, 0}, {KC_E, false, 36, 0}, {KC_R, false, 37, 0}, {KC_T, false, 38, 0}, {KC_Y, false, 39, 0}, {KC_U, false, 40, 0}, {KC_I, false, 41, 0}, {KC_O, false, 42, 0}, {KC_P, false, 43, 0}, {KC_LBRC, false, 44, 0}, {KC_RBRC, false, 45, 0},
+    {KC_A,false,50,0}, {KC_S,false,51,0}, {KC_D,false,52,0}, {KC_F,false,53,0}, {KC_G,false,54,0}, {KC_H,false,55,0}, {KC_J,false,56,0}, {KC_K,false,57,0}, {KC_L,false,58,0}, {KC_SCLN,false,59,0}, {KC_QUOT,false,60,0}, {KC_NUHS,false,61,0},
+    {KC_Z,false,65,0}, {KC_X,false,66,0}, {KC_C,false,67,0}, {KC_V,false,68,0}, {KC_B,false,69,0}, {KC_N,false,70,0}, {KC_M,false,71,0}, {KC_COMM,false,72,0}, {KC_DOT,false,73,0}, {KC_SLSH,false,74,0}, {KC_RSFT,false,75,0},
+    {KC_SPC,false,80,0}
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     
     for (uint8_t i = 0; i < RGB_MATRIX_LED_COUNT; i++) {
-        if (tablo[i].keycode == keycode) {
+        if (key_array[i].keycode == keycode) {
             if (record->event.pressed){
-                tablo[i].pressed = true;
-                tablo[i].timer = timer_read();
+                key_array[i].pressed = true;
+                key_array[i].timer = timer_read();
             }
         }
     }
@@ -93,10 +97,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
 
     for (uint8_t i = 0; i < RGB_MATRIX_LED_COUNT; i++) {
-        if (tablo[i].pressed) {
-            rgb_matrix_set_color(tablo[i].index, 255, 255, 255);
-            if (timer_elapsed(tablo[i].timer) > MAX_DELAY) {
-                tablo[i].pressed = false;
+        if (key_array[i].pressed) {
+            rgb_matrix_set_color(key_array[i].index, 255, 255, 255);
+            if (timer_elapsed(key_array[i].timer) > MAX_DELAY) {
+                key_array[i].pressed = false;
             }
         }
     }
